207896b851cc57fe32c13ed05bd1b4ebb5602d82
Islam Nofl <islamnofl.official@gmail.com>
2023-03-05 02:49:59 +0200

    Update Macros.h

--- src/CleanCheat/Macros.h
+++ src/CleanCheat/Macros.h
@@ -12,6 +12,9 @@
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 
 #if USE_LOGGER
+#include <cstdio>
+#include <cstring>
+#include <cstdio>
 #define LOG(format, ...) std::printf("[%s:%s:%i] " format "\n", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__)
 #else
 #define LOG(format, ...)
